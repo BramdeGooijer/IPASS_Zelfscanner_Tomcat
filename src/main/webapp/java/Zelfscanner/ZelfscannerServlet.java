f901427b7dabe741eeeeaf0626b2dd0966f19902
Bram <degooijerbram@gmail.com>
2022-05-02 21:26:32 +0200

    initial commit

---
+++ src/main/webapp/java/Zelfscanner/ZelfscannerServlet.java
@@ -0,0 +1,16 @@
+package Zelfscanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/zelfscanner")
+public class ZelfscannerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hij doet het!");
+    }
+}
