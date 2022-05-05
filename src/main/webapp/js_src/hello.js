6c68dc66cb25ab5f552ad4ad3dd1f0d4b761d8c2
Bram <degooijerbram@gmail.com>
2022-05-05 12:12:22 +0200

    dingen

--- src/main/webapp/js_src/hello.js
+++ src/main/webapp/js_src/hello.js
@@ -16,23 +16,5 @@ if (navigator.mediaDevices.getUserMedia) {
 window.addEventListener('load', () => init());
 
 function init() {
-    let form = document.querySelector("#btnID2")
-    let handler = (event) => form.action = event.target.value
-
-    document.querySelector("#btnID").addEventListener("click", handler);
-    document.querySelector("#btnID2").addEventListener("click", handler);
-
-    form.addEventListener("submit", (event) => submitForm(event));
-}
-
-function submitForm(event) {
-    let form = document.querySelector("#btnID2");
-    let data = new URLSearchParams(new FormData(form));
-
-    fetch(form.action + "?" + data.toString(), {method: form.method})
-        .then(response => response.text())
-        .then(text => console.log(text))
-        .catch(error => console.log(error));
-
-    event.preventDefault();
+    console.log('hoi');
 }
