f62f98468788ae5b284f7baddc29071680d9332f
Bram <degooijerbram@gmail.com>
2022-06-04 14:44:39 +0200

    begin fetching voor tikkie api payment

---
+++ src/main/webapp/js_src/payment.js
@@ -0,0 +1,17 @@
+// console.log("hij doet het");
+//
+// var myHeaders = new Headers();
+// myHeaders.append("API-KEY", "nru7vQI56ZAoUCfeHVJINgVeUeDLqFsa");
+// myHeaders.append("nru7vQI56ZAoUCfeHVJINgVeUeDLqFsa", "true");
+// myHeaders.append("Access-Control-Allow-Origin", "*");
+//
+// var requestOptions = {
+//     method: 'POST',
+//     headers: myHeaders,
+//     redirect: 'follow'
+// };
+//
+// fetch("https://api-sandbox.abnamro.com/v2/tikkie/sandboxapps", requestOptions)
+//     .then(response => response.text())
+//     .then(result => console.log(result))
+//     .catch(error => console.log('error', error));
