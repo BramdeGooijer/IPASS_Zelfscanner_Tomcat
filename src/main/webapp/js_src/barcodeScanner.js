69de4d461935a6b783ebc366e7829f9a64ec16f9
Bram <degooijerbram@gmail.com>
2022-06-21 23:39:27 +0200

    herhaaldelijk scannen is nu mogelijk, de producten worden in de boodschappenlijst gezet, scaling is een beetje beter, camera moet alleen nog wel betere scaling krijgen

--- src/main/webapp/js_src/barcodeScanner.js
+++ src/main/webapp/js_src/barcodeScanner.js
@@ -3,8 +3,9 @@ export function loadQuagga() {
         inputStream : {
             name : "Live",
             type : "LiveStream",
-            target: document.querySelector("#videoElement")
+            target: document.querySelector("#videoTag")
         },
+        frequency: 1,
         decoder : {
             readers : ["code_128_reader"]
         }
@@ -16,10 +17,47 @@ export function loadQuagga() {
         console.log("Initialization finished. Ready to start");
         Quagga.start();
     });
+}
 
-    Quagga.onDetected(function(result) {
-        let last_code = result.codeResult.code;
-        Quagga.stop();
-        console.log(last_code);
-    });
+Quagga.onDetected(function(result) {
+    let last_code = result.codeResult.code;
+    console.log(last_code);
+    handleBarcode(last_code);
+});
+
+function handleBarcode(barcode) {
+    //fetch hier naar de resource die de info door geeft van het product
+
+    const ul = document.querySelector("#items");
+
+    const li = document.createElement("li");
+    li.classList.add("item");
+    li.appendChild(document.createTextNode(barcode));
+
+    const itemAmountDiv = document.createElement("div");
+    itemAmountDiv.classList.add("itemAmount");
+    li.appendChild(itemAmountDiv);
+
+    const priceDiv = document.createElement("div");
+    priceDiv.appendChild(document.createTextNode("€2000000000.00"));
+    priceDiv.classList.add("price");
+    const decreaseDiv = document.createElement("div");
+    decreaseDiv.appendChild(document.createTextNode("-"));
+    decreaseDiv.classList.add("decrease");
+    decreaseDiv.classList.add("itemBubble");
+    const amountDiv = document.createElement("div");
+    amountDiv.appendChild(document.createTextNode("0"));
+    amountDiv.classList.add("amount");
+    amountDiv.classList.add("itemBubble");
+    const increaseDiv = document.createElement("div");
+    increaseDiv.appendChild(document.createTextNode("+"));
+    increaseDiv.classList.add("increase");
+    increaseDiv.classList.add("itemBubble");
+
+    itemAmountDiv.appendChild(priceDiv);
+    itemAmountDiv.appendChild(decreaseDiv);
+    itemAmountDiv.appendChild(amountDiv);
+    itemAmountDiv.appendChild(increaseDiv);
+
+    ul.appendChild(li);
 }
