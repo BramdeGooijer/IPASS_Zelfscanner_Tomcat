e66227ba390c68785e2ec7360089309539cafcf3
Bram <degooijerbram@gmail.com>
2022-06-27 13:36:54 +0200

    backend resource voor het opvragen van een product

---
+++ src/main/java/Zelfscanner/Domeinmodel/ProductResponse.java
@@ -0,0 +1,9 @@
+package Zelfscanner.Domeinmodel;
+
+public class ProductResponse {
+    public double prijs;
+    public String naam;
+    public String beschrijving;
+    public String barcode;
+    public String foutmelding;
+}
---
+++ src/main/java/Zelfscanner/Servlets/ProductResource.java
@@ -0,0 +1,40 @@
+package Zelfscanner.Servlets;
+
+import Zelfscanner.Domeinmodel.Product;
+import Zelfscanner.Domeinmodel.ProductResponse;
+import Zelfscanner.Domeinmodel.Winkel;
+import javassist.NotFoundException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/product")
+public class ProductResource {
+    Winkel winkel = Winkel.getWinkel();
+
+    @Path("{barcode}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getProductByBarcode(@PathParam("barcode") String barcode) throws NotFoundException {
+        try {
+            Product product = winkel.getProductByBarcode(barcode);
+
+            ProductResponse response = new ProductResponse();
+            response.prijs = product.getPrijs();
+            response.naam = product.getNaam();
+            response.beschrijving = product.getBeschrijving();
+            response.barcode = product.getBarcode();
+
+            return Response.ok(response).build();
+        } catch (NotFoundException e) {
+            ProductResponse response = new ProductResponse();
+            response.foutmelding = e.getMessage();
+
+            return Response.status(404).entity(response).build();
+        }
+    }
+}
