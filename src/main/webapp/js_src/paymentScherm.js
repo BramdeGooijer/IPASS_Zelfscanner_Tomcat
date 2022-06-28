c5abb8aa2d06084bbcbf2e05b58cd062cebde68f
Bram <degooijerbram@gmail.com>
2022-06-28 13:54:26 +0200

    betaalscherm met witte achtergrond recap wordt gecleared maar de variabele boodschappenlijst nog niet

--- src/main/webapp/js_src/paymentScherm.js
+++ src/main/webapp/js_src/paymentScherm.js
@@ -2,10 +2,12 @@ import {boodschappenlijst} from "./barcodeScanner.js";
 
 let paymentBtn = document.getElementById('paymentBtn');
 let paymentScherm = document.querySelector('#paymentContainer');
+let witScherm = document.querySelector('#whiteBackground');
 let paidBtn = document.querySelector('#betaaltBtn');
 
 paymentBtn.addEventListener('click', toggle);
-paidBtn.addEventListener('click', toggle)
+paidBtn.addEventListener('click', toggle);
+paidBtn.addEventListener('click', clear);
 
 
 function toggle() {
@@ -13,5 +15,14 @@ function toggle() {
         window.alert('scan eerst iets');
     } else {
         paymentScherm.classList.toggle('schuif-in');
+        witScherm.classList.toggle('schuif-in');
     }
+}
+
+function clear() {
+    let ul = document.querySelector('#ProductRecapList');
+
+    ul.innerHTML = '';
+
+    boodschappenlijst = [];
 }
