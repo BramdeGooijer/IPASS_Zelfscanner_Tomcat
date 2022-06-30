fb8b670ce0aebfb0c4eec827b853e8aa261f13e3
Bram <degooijerbram@gmail.com>
2022-06-30 16:30:12 +0200

    fixed errors for persistence

--- src/main/java/Zelfscanner/Servlets/MyServletContextListener.java
+++ src/main/java/Zelfscanner/Servlets/MyServletContextListener.java
@@ -1,11 +1,14 @@
 package Zelfscanner.Servlets;
 
 import Zelfscanner.Persistentie.PersistenceManager;
+import reactor.core.scheduler.Schedulers;
+import reactor.netty.http.HttpResources;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import java.io.IOException;
+import java.time.Duration;
 
 @WebListener
 public class MyServletContextListener implements ServletContextListener {
@@ -23,6 +26,10 @@ public class MyServletContextListener implements ServletContextListener {
     public void contextDestroyed(ServletContextEvent sce) {
         try {
             PersistenceManager.saveWinkelToAzure();
+
+            Schedulers.shutdownNow();
+            HttpResources.disposeLoopsAndConnectionsLater(Duration.ZERO, Duration.ZERO).block();
+
             System.out.println("De winkel is geupload");
         } catch (IOException e) {
             e.printStackTrace();
